8e45ea18080481cda420f96c49cb14c90d026ffa
JohnKim <johnkim@enbil.net>
2021-07-28 20:29:05 +0900

    project init

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+import MainPageComponent from './main/index.js';
+import { div } from 'prelude-ls';
+
+function App() {
+	return <MainPageComponent />;
+}
+
+export default App;
